49d27417f3cfbaf3aa439fe59d0bbeaaf60e17f1
Adam Hitchcock <adam@northisup.com>
2016-02-24 00:04:41 -0800

    Dynamic teachers POC

--- lindy/static920/views.py
+++ lindy/static920/views.py
@@ -3,17 +3,20 @@ from __future__ import absolute_import
 # External Libraries
 from django.views.generic import TemplateView
 
+from lindy.static920.models.teachers import TEACHERS
+
 
 class Index(TemplateView):
     template_name = 'index.jinja'
 
-
 class About(TemplateView):
     template_name = 'about.jinja'
 
-
 class Classes(TemplateView):
     template_name = 'classes.jinja'
+
+    def get_context_data(self, **kwargs):
+        return {'teachers': TEACHERS}
 
 class Dance(TemplateView):
     template_name = 'dance.jinja'
